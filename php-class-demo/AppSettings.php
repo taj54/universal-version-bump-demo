ab31cf283795ee7e8f4bb5927834da360174b97f
Taj <tajulislamj200@gmail.com>
2025-09-03 20:15:10 +0530

    chore(deps): update universal-version-bump to v0.13.2
    - Updated dependency version.
    - Added php-class-demo support.
    - Created AppSettings.php.
    - Created CHANGELOG.md.
    - Refactored bump targets.

---
+++ php-class-demo/AppSettings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Leantime\Core\Configuration;
+
+/**
+ * appSettings class - System appSettings
+ */
+class AppSettings
+{
+    public string $appVersion = '3.5.12';
+
+    public string $dbVersion = '3.4.9';
+}
